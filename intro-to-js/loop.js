add80beefa79fefb221b5661c46a247b188dd431
Murad Ahmed <muradahmed199771@gmail.com>
2022-05-07 21:08:07 +0600

    forEach loop added with while and for loop

--- intro-to-js/loop.js
+++ intro-to-js/loop.js
@@ -13,3 +13,8 @@ num = 30
 for(num; num > 1; num--){
     console.log (Math.ceil(num + Math.random(10)*100));
 }
+
+arr = [1,2,3,4,5,6,7,8,9,10];
+arr.forEach(element =>{
+    console.log(`This is Number: ${element}`);
+});
