cf600c442e5e42074ba0125cac9ca9f865f0608c
Murad Ahmed <muradahmed199771@gmail.com>
2022-06-24 22:35:21 +0600

    reverse string done

---
+++ MoreJsProblem/reverseString.js
@@ -0,0 +1,15 @@
+const ReverseString = (str) =>{
+    let reverseStr = "";
+    for(let i = (str.length)-1; i >= 0; i--){
+        reverseStr += str[i]
+    }
+    return reverseStr
+}
+
+const hello = ReverseString("Hello!")
+
+console.log("reverse result is: ", hello)
+
+const actual = ReverseString(hello);
+
+console.log("actual string was: ", actual)
